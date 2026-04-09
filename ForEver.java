06cf6b7bfba47dd9ac7dc4404375173fa1395cca
13020240062-tech <13020240062@student.umi.ac.id>
2026-04-10 00:05:10 +0800

    Add files via upload

---
+++ ForEver.java
@@ -0,0 +1,20 @@
+/*Nim : 13020240062
+ Nama :	Muhamad Febriyan Hazlin
+ Tanggal & Waktu : 09 April 2026 / 10.05
+*/
+public class ForEver { 
+ 
+ /** 
+  * @param args 
+  */ 
+ public static void main(String[] args) { 
+  // TODO Auto-generated method stub 
+  /* Program */ 
+  System.out.println("Program akan looping, akhiri dengan ^c"); 
+  while (true) 
+  { System.out.print ("Print satu baris ....\n"); 
+ 
+  } 
+ 
+ } 
+}
